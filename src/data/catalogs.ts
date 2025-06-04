59f35852b49f73b148b713edc65e354d36cc9098
DanilThais <heolamdanilothais@gmail.com>
2025-06-03 21:47:32 -0300

    Update catalog data

--- src/data/catalogs.ts
+++ src/data/catalogs.ts
@@ -1,111 +1,28 @@
 import { Catalog } from "../types";
 
 export const catalogs: Catalog[] = [
-  // Perfumes Femininos
   {
-    id: "perfume-feminino-1",
-    categoryId: "perfumes-femininos",
-    title: "Coleção Primavera 2025",
-    description: "Fragrâncias florais e frutais para a estação mais colorida do ano",
-    thumbnailUrl: "https://images.pexels.com/photos/4041392/pexels-photo-4041392.jpeg",
-    pdfUrl: "https://www.africau.edu/images/default/sample.pdf"
+    id: "lattafa",
+    categoryId: "perfumes",
+    title: "Lattafa",
+    description: "Catálogo completo de perfumes Lattafa",
+    thumbnailUrl: "https://images.pexels.com/photos/965989/pexels-photo-965989.jpeg",
+    pdfUrl: "https://drive.google.com/file/d/1McTwe-9hcxxZNFJquPzqxyMepKb0at6e/view?usp=drive_link"
   },
   {
-    id: "perfume-feminino-2",
-    categoryId: "perfumes-femininos",
-    title: "Linha Sofisticação",
-    description: "Perfumes exclusivos para ocasiões especiais",
-    thumbnailUrl: "https://images.pexels.com/photos/755992/pexels-photo-755992.jpeg",
-    pdfUrl: "https://www.orimi.com/pdf-test.pdf"
-  },
-  
-  // Perfumes Masculinos
-  {
-    id: "perfume-masculino-1",
-    categoryId: "perfumes-masculinos",
-    title: "Essência Masculina",
-    description: "Fragrâncias marcantes para o homem moderno",
+    id: "maison-alhambra",
+    categoryId: "perfumes",
+    title: "Maison Alhambra",
+    description: "Coleção exclusiva Maison Alhambra",
     thumbnailUrl: "https://images.pexels.com/photos/3766121/pexels-photo-3766121.jpeg",
-    pdfUrl: "https://www.africau.edu/images/default/sample.pdf"
+    pdfUrl: "https://drive.google.com/file/d/17WqYs4Kl2Ad7__V5F8JYJehT6NCAN-aG/view?usp=drive_link"
   },
   {
-    id: "perfume-masculino-2",
-    categoryId: "perfumes-masculinos",
-    title: "Coleção Executivo",
-    description: "Perfumes sofisticados para o ambiente profissional",
-    thumbnailUrl: "https://images.pexels.com/photos/3799103/pexels-photo-3799103.jpeg",
-    pdfUrl: "https://www.orimi.com/pdf-test.pdf"
-  },
-  
-  // Hidratantes
-  {
-    id: "hidratante-1",
-    categoryId: "hidratantes",
-    title: "Hidratação Profunda",
-    description: "Cremes hidratantes para todos os tipos de pele",
+    id: "diversos",
+    categoryId: "perfumes",
+    title: "Diversos",
+    description: "Catálogo de produtos diversos",
     thumbnailUrl: "https://images.pexels.com/photos/3321416/pexels-photo-3321416.jpeg",
-    pdfUrl: "https://www.africau.edu/images/default/sample.pdf"
-  },
-  {
-    id: "hidratante-2",
-    categoryId: "hidratantes",
-    title: "Linha Verão",
-    description: "Hidratantes especiais para o verão",
-    thumbnailUrl: "https://images.pexels.com/photos/3735634/pexels-photo-3735634.jpeg",
-    pdfUrl: "https://www.orimi.com/pdf-test.pdf"
-  },
-  
-  // Maquiagem
-  {
-    id: "maquiagem-1",
-    categoryId: "maquiagem",
-    title: "Tendências 2025",
-    description: "As últimas novidades em maquiagem",
-    thumbnailUrl: "https://images.pexels.com/photos/457701/pexels-photo-457701.jpeg",
-    pdfUrl: "https://www.africau.edu/images/default/sample.pdf"
-  },
-  {
-    id: "maquiagem-2",
-    categoryId: "maquiagem",
-    title: "Maquiagem Profissional",
-    description: "Produtos para maquiadores profissionais",
-    thumbnailUrl: "https://images.pexels.com/photos/2113855/pexels-photo-2113855.jpeg",
-    pdfUrl: "https://www.orimi.com/pdf-test.pdf"
-  },
-  
-  // Skincare
-  {
-    id: "skincare-1",
-    categoryId: "skincare",
-    title: "Rotina Completa",
-    description: "Produtos para uma rotina completa de skincare",
-    thumbnailUrl: "https://images.pexels.com/photos/3785147/pexels-photo-3785147.jpeg",
-    pdfUrl: "https://www.africau.edu/images/default/sample.pdf"
-  },
-  {
-    id: "skincare-2",
-    categoryId: "skincare",
-    title: "Tratamentos Específicos",
-    description: "Produtos para tratamentos específicos da pele",
-    thumbnailUrl: "https://images.pexels.com/photos/3762876/pexels-photo-3762876.jpeg",
-    pdfUrl: "https://www.orimi.com/pdf-test.pdf"
-  },
-  
-  // Cabelos
-  {
-    id: "cabelos-1",
-    categoryId: "cabelos",
-    title: "Tratamentos Capilares",
-    description: "Produtos para todos os tipos de cabelo",
-    thumbnailUrl: "https://images.pexels.com/photos/3993449/pexels-photo-3993449.jpeg",
-    pdfUrl: "https://www.africau.edu/images/default/sample.pdf"
-  },
-  {
-    id: "cabelos-2",
-    categoryId: "cabelos",
-    title: "Linha Profissional",
-    description: "Produtos profissionais para cabelos",
-    thumbnailUrl: "https://images.pexels.com/photos/3993259/pexels-photo-3993259.jpeg",
-    pdfUrl: "https://www.orimi.com/pdf-test.pdf"
+    pdfUrl: "https://drive.google.com/file/d/1xldP6m-Dd0WsfG6V0pRhYJVAJdbF4pOK/view?usp=drive_link"
   }
 ];
