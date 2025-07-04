dbe8c7286630fe3ab3e57c3207076b0e595de12e
DanilThais <heolamdanilothais@gmail.com>
2025-07-04 17:33:21 -0300

    Adicionar todas as novas categorias e catálogos

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -20,6 +20,42 @@ export const categories: Category[] = [
     image: "https://images.pexels.com/photos/1190829/pexels-photo-1190829.jpeg",
   },
   {
+    id: "internacionais",
+    name: "Internacionais",
+    description: "Catálogo de perfumes internacionais",
+    image: "https://images.pexels.com/photos/1961795/pexels-photo-1961795.jpeg",
+  },
+  {
+    id: "armaf",
+    name: "Armaf",
+    description: "Catálogo completo de perfumes Armaf",
+    image: "https://images.pexels.com/photos/3738673/pexels-photo-3738673.jpeg",
+  },
+  {
+    id: "arabic-collection",
+    name: "Arabic Collection",
+    description: "Coleção árabe de perfumes exclusivos",
+    image: "https://images.pexels.com/photos/3738674/pexels-photo-3738674.jpeg",
+  },
+  {
+    id: "al-wataniah",
+    name: "Al Wataniah",
+    description: "Catálogo completo de perfumes Al Wataniah",
+    image: "https://images.pexels.com/photos/3738675/pexels-photo-3738675.jpeg",
+  },
+  {
+    id: "al-haramain",
+    name: "Al Haramain",
+    description: "Catálogo completo de perfumes Al Haramain",
+    image: "https://images.pexels.com/photos/3738676/pexels-photo-3738676.jpeg",
+  },
+  {
+    id: "afnan",
+    name: "Afnan",
+    description: "Catálogo completo de perfumes Afnan",
+    image: "https://images.pexels.com/photos/3738677/pexels-photo-3738677.jpeg",
+  },
+  {
     id: "diversos",
     name: "Diversos",
     description: "Catálogo de produtos diversos",
